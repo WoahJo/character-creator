68a6c3be6b5b89a1c0f2dd816bd7412226dcd71d
WoahJo <ccnettacc@gmail.com>
2023-09-08 20:44:45 -0400

    rendering the attributes based on radio selection

--- src/App.jsx
+++ src/App.jsx
@@ -3,59 +3,19 @@ import './App.css'
 import Form from './Form';
 
 function App() {
-  const [attr1, setAttr1] = useState('');
-  const [attr2, setAttr2] = useState('');
-  const [attr3, setAttr3] = useState('');
-  const [attr4, setAttr4] = useState('');
-  const [attr5, setAttr5] = useState('');
-  const [attr6, setAttr6] = useState('');
-  const [attr7, setAttr7] = useState('');
-
-  const [id1, setId1] = useState('');
-  const [id2, setId2] = useState('');
-  const [id3, setId3] = useState('');
-  const [id4, setId4] = useState('');
-  const [id5, setId5] = useState('');
-  const [id6, setId6] = useState('');
-  const [id7, setId7] = useState('');
+  const [attributes, setAttributes] = useState([])
 
 
   // ALSO NEED STAT NUMBERS!
 
   const onDnd = () => {
     console.log('Setting Dnd 6');
-    setAttr1('Strength');
-    setId1('Strength');
-    setAttr2('Constitution');
-    setId2('Constitution');
-    setAttr3('Dexterity');
-    setId3('Dexerity');
-    setAttr4('Intelligence');
-    setId4('Intelligence');
-    setAttr5('Wisdom');
-    setId5('Wisdom');
-    setAttr6('Charisma');
-    setId6('Charisma');
-    setAttr7(null);
-    setId7(null);
+    setAttributes(['Strength', 'Constitution', 'Dexterity', 'Intelligence', 'Wisdom', 'Charisma']);
   }
 
   const onSpec = () => {
     console.log('Setting SPECIAL');
-    setAttr1('Strength');
-    setId1('Strength2');
-    setAttr2('Perception');
-    setId2('Perception');
-    setAttr3('Endurance');
-    setId3('Endurance');
-    setAttr4('Charisma');
-    setId4('Charisma2');
-    setAttr5('Intelligence');
-    setId5('Intelligence2')
-    setAttr6('Agility');
-    setId6('Agility');
-    setAttr7('Luck');
-    setId7('Luck');
+    setAttributes(['Strength', 'Perception', 'Endurance', 'Charisma', 'Intelligence', 'Agility', 'Luck']);
   }
   return (
     <>
@@ -69,7 +29,7 @@ function App() {
           <label htmlFor='spec'>S.P.E.C.I.A.L (Fallout)</label>
         </div>
       </div>
-      <Form attributes={[{attr: attr1, id: id1}, {attr: attr2, id: id2}, {attr: attr3, id: id3}, {attr: attr4, id: id4}, {attr: attr5, id: id5}, {attr: attr6, id: id6}, {attr: attr7, id: id7}]} />
+      <Form attributes={attributes} />
     </>
   )
 }
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,18 +1,43 @@
 // import React from "react";
 import PropTypes from "prop-types";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 export default function Form(props) {
     const { attributes } = props; 
-    // const statItems = attributes.map((prop) => 
-    //     <h1 key={}
-    // )
+    const [stat1, setStat1] = useState(0); 
+
+    const statItems = attributes.map((prop) => 
+        <div key={uuidv4()} id="formValues">
+            <p>{prop}</p>
+            <input type="number" onClick={(e)=> handlePoint(e)}/>
+        </div>
+        )
+        console.log(statItems);
+
+    const handlePoint = (e) => {
+        const attr = e.target.previousSibling.textContent;
+
+        switch (true) {
+            case attr === 'Strength':
+                setStat1(stat1 + e.target.value)
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+
     return (
         <>
-            {attributes.map((prop) => (
+            {statItems}
+            {/* {attributes.map((prop) => (
                 <div key={prop.id} id="formValues">
                     <h1>{prop.attr}</h1>
                     <input type="number" />
                 </div>
-            ))} 
+            ))}  */}
         </>
     );
 }
